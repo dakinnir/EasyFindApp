43ee6fe5e5c34762e26a8991332a11ee52f5af06
Daniel Akinniranye <dakinnir@gmail.com>
2022-09-18 12:32:33 -0400

    Initial Commit

---
+++ EasyFind/EasyFindApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyFindApp.swift
+//  EasyFind
+//
+//  Created by Daniel Akinniranye on 9/18/22.
+//
+
+import SwiftUI
+
+@main
+struct EasyFindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
