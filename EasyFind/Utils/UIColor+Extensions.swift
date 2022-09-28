d8ea47f9a4e9a8d99040e203901a5e703a6aa444
Daniel Akinniranye <dakinnir@gmail.com>
2022-09-28 15:03:40 -0400

    Added extension property to the UIColor class

---
+++ EasyFind/Utils/UIColor+Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor+Extensions.swift
+//  EasyFind
+//
+//  Created by Daniel Akinniranye on 9/28/22.
+//
+
+import Foundation
+import UIKit
+
+
+extension UIColor {
+    static let mainColor = UIColor(red: 117/255, green: 124/255, blue: 243/255, alpha: 1.0)
+}
