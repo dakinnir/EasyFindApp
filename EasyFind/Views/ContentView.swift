76cf56f18a2c1ec998aa21909ba81480f28e6c1b
Daniel Akinniranye <dakinnir@gmail.com>
2022-09-30 10:28:52 -0400

    Developed a basic tab view.

--- EasyFind/Views/ContentView.swift
+++ EasyFind/Views/ContentView.swift
@@ -8,12 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        TabView {
+            ExplorePageView()
+                .tabItem {
+                Image(systemName: "hand.tap.fill")
+                Text("Explore")
+            }
+            Text("Bookmark Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded)) .tabItem {
+                    Image(systemName: "bookmark.circle.fill")
+                    Text("Bookmark")
+                }
+            Text("Recommendations Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded)) .tabItem {
+                    Image(systemName: "star.circle.fill")
+                    Text("Recommendations")
+                }
+            
+            Text("Notifications Tab")
+                .font(.system(size: 30, weight: .bold, design: .rounded)) .tabItem {
+                    Image(systemName: "bell.circle.fill")
+                    Text("Notifications")
+                }
         }
         .padding()
     }
@@ -22,5 +43,40 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ExplorePageView: View {
+    
+    @State private var searchEntry: String = ""
+    var body: some View {
+        NavigationView {
+            VStack {
+                SearchBarView(text: $searchEntry, searchBarPlaceholderText: "Search...")
+                Spacer()
+                Text("Explore Tab")
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                Spacer()
+            }
+        }
+    }
+}
+
+
+struct SearchBarView: UIViewRepresentable {
+    
+    @Binding var text: String
+    var searchBarPlaceholderText: String
+
+    func makeUIView(context: Context) -> UISearchBar {
+        let searchBar = UISearchBar()
+        searchBar.searchBarStyle = .minimal
+        searchBar.autocapitalizationType = .none
+        searchBar.placeholder = searchBarPlaceholderText
+        return searchBar
+    }
+    
+    func updateUIView(_ uiView: UISearchBar, context: Context) {
+        uiView.text = text
     }
 }
