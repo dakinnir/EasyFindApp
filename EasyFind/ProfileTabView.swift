821608085ff0864cae0ecc2823bae97b3ba4a34b
Daniel Akinniranye <dakinnir@gmail.com>
2022-10-04 11:26:22 -0400

    Added gitignore file

---
+++ EasyFind/ProfileTabView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileTabView.swift
+//  EasyFind
+//
+//  Created by Daniel Akinniranye on 9/30/22.
+//
+
+import SwiftUI
+
+struct ProfileTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileTabView()
+    }
+}
