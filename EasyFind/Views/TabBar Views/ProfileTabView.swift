6e4fbb1eae30468a85112de9f3be8cdf6242273c
Daniel Akinniranye <dakinnir@gmail.com>
2022-10-05 00:47:11 -0400

    Created a file for the profile tab view.

---
+++ EasyFind/Views/TabBar Views/ProfileTabView.swift
@@ -0,0 +1,80 @@
+//
+//  ProfilePageView.swift
+//  EasyFind
+//
+//  Created by Daniel Akinniranye on 10/4/22.
+//
+
+import SwiftUI
+
+enum ProfileTabMenuItems: String, CaseIterable {
+    case account = "account"
+    case settings = "settings"
+    case notifications = "notifications"
+    case logout = "logout"
+    
+    var description: String {
+        return self.rawValue.capitalized
+    }
+}
+                                
+                                
+struct ProfileTabView: View {
+    
+    // MARK: - Properties
+    @EnvironmentObject var authViewModel: AuthenticationViewModel
+
+    private let profileLinkNames = ProfileTabMenuItems.allCases
+    
+    // MARK: - Body
+    var body: some View {
+        NavigationView {
+            VStack(spacing: 0) {
+                List(profileLinkNames, id: \.self) { item in
+                    if item == .logout {
+                        Button {
+                            DispatchQueue.main.async {
+                                authViewModel.logoutUser()
+                            }
+                        } label: {
+                            Text(item.description)
+                                .font(.body)
+                        }
+                    } else {
+                        NavigationLink {
+                            Text(item.description)
+                        } label: {
+                            Text(item.description)
+                                .font(.body)
+                        }
+                    }
+                }
+                .listStyle(.inset)
+            }
+            
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarLeading, content: {
+                    // Text("Premium Member")
+                    Text("Painter")
+                        .font(.body)
+                        .foregroundColor(Color(.systemGray))
+                })
+                
+                ToolbarItem(placement: .navigationBarTrailing, content: {
+                    Image("painter")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                        .clipShape(Circle()) // Clip the image to a circle
+                })
+            })
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+struct ProfileTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileTabView()
+    }
+}
