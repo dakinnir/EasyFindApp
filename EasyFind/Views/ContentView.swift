2f95cdf0f81ee6e4c04fc88c9cfabb33ead0fcf9
Daniel Akinniranye <dakinnir@gmail.com>
2022-10-06 12:07:35 -0400

    New changes.

--- EasyFind/Views/ContentView.swift
+++ EasyFind/Views/ContentView.swift
@@ -10,39 +10,43 @@ import SwiftUI
 struct ContentView: View {
     
     @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var authViewModel: AuthenticationViewModel
     
     var body: some View {
-        
+        Group {
+            if authViewModel.currentUserSession != nil {
+                homeTabView
+            } else {
+                WelcomePageView()
+            }
+        }
+    }
+    
+    private var homeTabView: some View {
         TabView {
             ExplorePageView()
                 .tabItem {
                 Image(systemName: "magnifyingglass.circle.fill")
                 Text("Explore")
             }
-//            Text("Bookmark Tab")
-//                .font(.system(size: 30, weight: .bold, design: .rounded)) .tabItem {
-//                    Image(systemName: "bookmark.circle.fill")
-//                    Text("Bookmark")
-//                }
-            
             Text("Messages Tab")
                 .font(.system(size: 30, weight: .bold, design: .rounded)) .tabItem {
                     Image(systemName: "envelope")
                     Text("Messages")
                 }
             
-            Text("Work Tab")
+            Text("Calendar Tab")
                 .font(.system(size: 30, weight: .bold, design: .rounded)) .tabItem {
-                    Image(systemName: "checkmark.circle.fill")
-                    Text("Work")
+                    Image(systemName: "calendar.circle.fill")
+                    Text("Calendar")
                 }
             
             Text("Notifications Tab")
                 .font(.system(size: 30, weight: .bold, design: .rounded)) .tabItem {
-                    Image(systemName: "bell.circle.fill")
+                    Image(systemName: "bell.fill")
                     Text("Notifications")
                 }
-            
+        
             ProfileTabView()
                 .font(.system(size: 30, weight: .bold, design: .rounded)) .tabItem {
                     Image(systemName: "person.circle.fill")
@@ -55,43 +59,10 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct ExplorePageView: View {
-    
-    @State private var searchEntry: String = ""
-    var body: some View {
-        NavigationView {
-            VStack {
-                SearchBarView(text: $searchEntry, searchBarPlaceholderText: "Search...")
-                    .padding(.horizontal)
-                Spacer()
-                Text("Explore Tab")
-                        .font(.system(size: 30, weight: .bold, design: .rounded))
-                Spacer()
-            }
-            .navigationTitle("Explore")
-        }
+            .environmentObject(AuthenticationViewModel())
     }
 }
 
 
-struct SearchBarView: UIViewRepresentable {
-    
-    @Binding var text: String
-    var searchBarPlaceholderText: String
 
-    func makeUIView(context: Context) -> UISearchBar {
-        let searchBar = UISearchBar()
-        searchBar.searchBarStyle = .minimal
-        searchBar.heightAnchor.constraint(equalToConstant: 40)
-        searchBar.autocapitalizationType = .none
-        searchBar.placeholder = searchBarPlaceholderText
-        return searchBar
-    }
-    
-    func updateUIView(_ uiView: UISearchBar, context: Context) {
-        uiView.text = text
-    }
-}
+
